c61d422ac6fce01f02e480c121d077d4bcfd219b
YudGir <yudhagirsang07@gmail.com>
2026-03-26 10:24:32 +0700

    menambah kode program first-try di rekursif.cpp

--- rekursif.cpp
+++ rekursif.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void hitungMundur(int n) {
+    if (n == 0) {
+        return;
+    } 
+
+    cout << n << " ";
+
+    hitungMundur(n-1);
+}
+
 int main () {
-    
+    int angkaUser;
+    cout << "Masukkan angka yang mau Anda lihat hitung-mundurnya: ";
+    cin >> angkaUser;
+
+    hitungMundur(angkaUser);
+
+    return 0;
 }
